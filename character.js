cd1a217ca8dbcb0ee054915176f3ba5df547032c
Tom Saillard <tom.saillard@ynov.com>
2023-04-11 09:02:24 +0200

    make architecture

---
+++ character.js
@@ -0,0 +1,25 @@
+class Character {
+    PhysicalAttack;
+    CurrentHealthPoint;
+    MaxHealthPoint;
+    CurrentMana;
+    MaxMana;
+    Velocity;
+    Armor;
+    Level;
+    CurrentGold; 
+
+    constructor(physicalAttack, currentHealthPoint, maxHealthPoint, currentMana, maxMana, velocity, armor, level, currentGold) {
+        this.PhysicalAttack = physicalAttack;
+        this.CurrentHealthPoint = currentHealthPoint;
+        this.MaxHealthPoint = maxHealthPoint;
+        this.CurrentMana = currentMana;
+        this.MaxMana = maxMana;
+        this.Velocity = velocity;
+        this.Armor = armor;
+        this.Level = level;
+        this.CurrentGold = currentGold;
+    }
+}
+
+
