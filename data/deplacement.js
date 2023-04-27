f682e2268635ce0a7706f70bf699ed9ba69103db
Tom Saillard <tom.saillard@ynov.com>
2023-04-27 08:28:59 +0200

    Advance on the movement

--- data/deplacement.js
+++ data/deplacement.js
@@ -13,6 +13,11 @@ const keys = {
     },
 };
 
+if (keys.z.pressed){
+    background.position.y = background.position.y + 3
+    
+}
+
 
 window.addEventListener('keydown'), (e) => {
     switch (e.key) {
@@ -30,3 +35,48 @@ window.addEventListener('keydown'), (e) => {
             break;
     }
 }
+
+window.addEventListener('keyup'), (e) => {
+    switch (e.key) {
+        case 'z':
+            keys.w.pressed = false;
+            break;
+        case 'q':
+            keys.a.pressed = false;
+            break;
+        case 's':
+            keys.s.pressed = false;
+            break;
+        case 'd':
+            keys.d.pressed = false;
+            break;
+    }
+}
+
+function animate (
+    window.requestAnimationFrame(animate);
+
+)
+
+if ()
+
+
+class Sprite {
+    constructor(position, velocity, image){
+        this.position = position;
+        this.velocity = velocity;
+        this.image = image;
+    }
+}
+
+draw () {
+    c.drawImage(this.image,this.position.x ,this.position.y);
+}
+
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    image: image
+})
