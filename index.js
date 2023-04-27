10a73016018e55348876fb81144b1801df154ac1
noa <nxa.drxuineau@gmail.com>
2023-04-27 08:26:53 +0200

    add player

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 1920
-canvas.height = 1080
+canvas.width = 1024
+canvas.height = 576
 
 c.fillStyle = 'red';
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 const image = new Image();
 image.src = './image/Ile_Prairie_LM.png';
-console.log(image);
 
 const playerImage = new Image();
-playerImage.src = './image/player.png';
+playerImage.src = './image/playerDown.png';
 
 image.onload = () => {
-    c.drawImage(image, 0, 0);
-    c.drawImage(playerImage, 300, 0);
-    }
+    c.drawImage(image,-155, -135);
+    c.drawImage(playerImage,0,0,playerImage.width/4,playerImage.height, canvas.width/2 - playerImage.width/4/2, canvas.height/2 - playerImage.height/2, playerImage.width/4, playerImage.height);
+}
+
+
 
 
